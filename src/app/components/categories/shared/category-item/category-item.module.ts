9f7b19a6aeeac100d7f32b8257af021363820bad
brantesdavi <davi.brantes@gmail.com>
2023-08-04 00:35:30 -0300

    feat: criado componente de categorias

---
+++ src/app/components/categories/shared/category-item/category-item.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CategoryItemComponent } from './category-item.component';
+
+
+
+@NgModule({
+  declarations: [
+    CategoryItemComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class CategoryItemModule { }
