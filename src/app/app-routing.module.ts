0cc179f76c2337d320900adfe7cc2a6fde7e868f
brantesdavi <davi.brantes@gmail.com>
2023-08-09 21:50:54 -0300

    feat: ultimos ajustes dos facilities

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
+
 const routes: Routes = [];
+RouterModule.forRoot(routes, { useHash: false })
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
